169267b43ca4125fbdcc291f137954d74cb9bfd6
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2024-09-10 10:29:33 -0400

    set to main kjv

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -21,7 +21,7 @@ fn main() {
 	tauri::Builder::default()
 		.setup(|app| {
 			let resource_path = app.path_resolver()
-				.resolve_resource("resources/small_kjv.txt")
+				.resolve_resource("resources/kjv.txt")
 				.expect("Failed to retrieve `kjv.txt` resource");
 
 			let mut file = std::fs::File::open(&resource_path).unwrap();
