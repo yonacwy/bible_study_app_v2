db538c65978b67c30ed5698f99e2607714a56ecb
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2024-10-27 19:15:15 -0400

    changed gitignore

--- ui/dist/utils/extensions.js
+++
@@ -1,9 +0,0 @@
-Element.prototype.appendElement = function (key, builder) {
-    let child = document.createElement(key);
-    if (builder !== undefined) {
-        builder(child);
-    }
-    this.appendChild(child);
-    return this;
-};
-export {};
--- ui/src/page_scripts/bible_note_page.ts
+++ ui/src/page_scripts/bible_note_page.ts
@@ -1,4 +1,4 @@
-import * as utils from "../utils.js";
+import * as utils from "../utils/index.js";
 import * as pages from "./pages.js";
 
 export function run()
