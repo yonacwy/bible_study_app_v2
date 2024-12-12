ace87c5d8ab25370bd232af732d080dc0e59fe50
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2024-12-12 16:23:08 -0500

    less working!!!

---
+++ ui/src/utils/redirect_console.js
@@ -0,0 +1,14 @@
+
+const redirect = false;
+if(redirect)
+{
+    console['log'] = (msg) => window.__TAURI__.core.invoke('debug_print', {message: msg});
+    console['debug'] = (msg) => window.__TAURI__.core.invoke('debug_print', {message: msg});
+    console['info'] = (msg) => window.__TAURI__.core.invoke('debug_print', {message: msg});
+    console['warn'] = (msg) => window.__TAURI__.core.invoke('debug_print', {message: msg});
+    console['error'] = (msg) => window.__TAURI__.core.invoke('debug_print', {message: msg});
+}
+
+less = {
+    errorReporting: 'console',
+}
