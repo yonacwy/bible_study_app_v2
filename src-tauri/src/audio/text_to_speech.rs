2a0f60f6061e30ec6ce06ecae08139f8cd025f13
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2025-02-02 23:48:59 -0500

    Update text_to_speech.rs

--- src-tauri/src/audio/text_to_speech.rs
+++ src-tauri/src/audio/text_to_speech.rs
@@ -1,4 +1,32 @@
+use std::sync::{Arc, Mutex};
+
 use tts::*;
+
+pub struct AppTts
+{
+    tts: Arc<Mutex<Tts>>
+}
+
+impl AppTts
+{
+    pub fn new() -> Result<Self, String>
+    {
+        let tts = match Tts::default()
+        {
+            Ok(ok) => ok,
+            Err(err) => return Err(err.to_string())
+        };
+
+        Ok(Self {
+            tts: Arc::new(Mutex::new(tts))
+        })
+    }
+
+    pub fn speak(&self, text: &str)
+    {
+        self.tts.lock().unwrap().speak(text, true);
+    }
+}
 
 pub fn test() -> Result<Tts, Error>
 {
