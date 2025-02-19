9ed5a5695f3ae1553575db3642661b4c34770403
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2025-02-19 02:16:36 -0500

    Update player_behavior.ts

--- ui/src/popups/player_behavior.ts
+++ ui/src/popups/player_behavior.ts
@@ -1,13 +1,19 @@
 import { ChapterIndex, ReferenceLocation, VerseRange } from "../bindings"
 
+export type PlayerSegment = {
+    chapter_index: ChapterIndex,
+    verse_range: VerseRange | null
+};
+
 export type PlayerBehavior = {
     name: string,
     image_src: string,
-    get_next: (count: number, time: number) => ChapterIndex | VerseRange | null,
+    get_next: (index: number) => PlayerSegment,
 };
 
 export class ReaderSchedular
 {
-    private current_behavior: PlayerBehavior
-    private current_count: number
+    private current_behavior: PlayerBehavior;
+    private index: number;
+    private time: number;
 }
